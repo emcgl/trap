c1046a4072a905228883c0962e5c460efa24b51f
Marijn Verkerk <m.verkerk@erasmusmc.nl>
2015-09-03 09:37:16 +0000

    Corrected title / add color

--- includes/header.php
+++ includes/header.php
@@ -5,14 +5,14 @@ function gen_html_header($title) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>Transcriptomic Age Calculation - <?php echo $title;?></title>
+	<title>Transcriptomic Age Prediction - <?php echo $title;?></title>
 	<link rel="stylesheet" href="/css/tragca.css">
 	<script src="/js/tragca.js"></script>
 </head>
 <body>
 <div id="page">
 	<div id="top">
-		<h1 id="toptitle">Transcriptomic Age Calculation - <?php echo $title;?></h1>
+		<h1 id="toptitle"><span style="color: #92b6cf;">Tr</span>anscriptomic <span style="color: #92b6cf;">A</span>ge <span style="color: #92b6cf;">P</span>rediction - <?php echo $title;?></h1>
 	</div><!-- top -->
 	<div id="content">
 <?php 
