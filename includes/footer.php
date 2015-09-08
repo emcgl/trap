6b57632b4bdbef75725d839245470b7970a307f6
Marijn Verkerk <m.verkerk@erasmusmc.nl>
2015-09-08 15:03:09 +0000

    Added footer with logo's and text

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,11 @@
 </div><!-- content -->
 <div class="footer">
-
+<a href="http://depts.washington.edu/chargeco/wiki/Main_Page"><img class="chargelogo" src="/images/chargelogo.png"/></a>
+<a href="http://www.erasmusmc.nl"><img class="emclogo" src="/images/emclogo.png"/></a>
+<div id="footertext">
+<p>This website is developed by <a class="no-link" href="mailto:m.verkerk@erasmusmc.nl">Marijn Verkerk</a>.</p>
+<p>Copyright &copy; 2015 Genetic Laboratory<br/><a class="no-link" href="http://www.glimdna.org">Internal Medicine</a> - <a class="no-link" href="http://www.erasmusmc.nl">Erasmus MC</a><br/>All rights reserved.
+</div>
 </div>
 </div><!-- page -->
 </body>
