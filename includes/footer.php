f827ebbd54e1d0ceecd21dacb6f3d6b7c7e9cacd
Marijn Verkerk <m.verkerk@erasmusmc.nl>
2015-09-17 09:37:52 +0000

    Improved footer contents

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,13 @@
 </div><!-- content -->
 <div class="footer">
-<a href="http://depts.washington.edu/chargeco/wiki/Main_Page"><img class="chargelogo" src="/images/chargelogo.png"/></a>
 <a href="http://www.erasmusmc.nl"><img class="emclogo" src="/images/emclogo.png"/></a>
+<a href="http://depts.washington.edu/chargeco/wiki/Main_Page"><img class="chargelogo" src="/images/chargelogo.png"/></a>
 <div id="footertext">
 <p>This website is developed by <a class="no-link" href="mailto:m.verkerk@erasmusmc.nl"><span style="color: #92b6cf;">Marijn Verkerk</span></a>.</p>
-<p>Copyright &copy; 2015 <a class="no-link" href="http://www.glimdna.org"><span style="color: #92b6cf;">Genetic Laboratory</span></a><br/>Internal Medicine - <a class="no-link" href="http://www.erasmusmc.nl"><span style="color: #92b6cf;">Erasmus MC</a></span><br/>All rights reserved.
+<a class="no-link" href="http://www.glimdna.org">
+<p>Copyright &copy; 2015 Genetic Laboratory<br/>Internal Medicine - Erasmus MC<br/>
+<p>Visit our website: www.glimdna.org</p><br/>
+</a>
 </div>
 </div>
 </div><!-- page -->
