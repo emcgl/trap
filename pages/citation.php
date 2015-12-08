b2b252073498687cbbb3e8635835b3456b218bb2
Marijn Verkerk <m.verkerk@erasmusmc.nl>
2015-12-08 11:15:50 +0000

    Updated citing

--- pages/citation.php
+++ pages/citation.php
@@ -8,8 +8,8 @@ $user=$_SESSION['user'];
 <div class="view">
 <div class="txt">
 <p>The Transcriptomic Age Prediction Tool has been described in:<p> 
-<p><i>Peters MJ, Joehanes R, Pilling LC, Conneely K, Powell J et al. 2015: The transcriptional landscape of age in human peripheral blood. Nature Communications. 2015. DOI: . PMID: .</i><p> 
-<!--   <a href="" disable>Link to article</a> -->
+<p><i>Peters MJ, Joehanes R, Pilling LC, Conneely K, Powell J et al. The transcriptional landscape of age in human peripheral blood. Nature Communications. 2015 Oct 22;6:8570. DOI: 10.1038/ncomms9570.</i><p> 
+<a href="http://www.nature.com/ncomms/2015/151022/ncomms9570/full/ncomms9570.html">Link to article</a>
 </div>
 <?php 
 	echo "<div class=\"menu\">".PHP_EOL;
